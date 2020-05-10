d4befb1a4d9bc97ed96b372a234ecad208f9a8a6
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-05-10 12:25:09 -0300

    1.0 Version

--- install.bat
+++ install.bat
@@ -6,7 +6,7 @@ mkdir C:\URLBrute\logs
 mkdir C:\URLBrute\logs\sub_domains
 mkdir C:\URLBrute\logs\dirs
 
-copy main.py C:\URLBrute\urlbrute.py
+copy urlbrute.py C:\URLBrute\urlbrute.py
 copy wordlists C:\URLBrute\wordlists
 
 python3 -m pip install -r requirements.txt
--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 echo "Installing..."
 
+mkdir /usr/share/URLBrute
 mkdir /usr/share/URLBrute/logs
 mkdir /usr/share/URLBrute/logs/sub_domains
 mkdir /usr/share/URLBrute/logs/dirs
 
-cp main.py /usr/share/URLBrute/main.py
+cp urlbrute.py /usr/share/URLBrute/urlbrute.py
 mv wordlists /usr/share/URLBrute
 
-ln -s /usr/share/URLBrute/main.py /usr/bin/urlbrute
+ln -s /usr/share/URLBrute/urlbrute.py /usr/bin/urlbrute
 
 python3 -m pip install -r requirements.txt
 
