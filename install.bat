39affa1b159b26486da6de57ccb74f735a9093cf
ReddyyZ <41487053+ReddyyZ@users.noreply.github.com>
2020-05-13 17:15:08 -0300

    1.1 Version

--- install.bat
+++ install.bat
@@ -9,7 +9,10 @@ mkdir C:\URLBrute\logs\dirs
 copy urlbrute.py C:\URLBrute\urlbrute.py
 copy wordlists C:\URLBrute\wordlists
 
+setx WORDLISTS "C:\URLBrute\wordlists" /M
+
 python3 -m pip install -r requirements.txt
 
 echo URLBrute installed!
 echo Please add C:\URLBrute to path
+echo To see wordlists try: dir %%WORDLISTS%%
--- install.sh
+++ install.sh
@@ -8,8 +8,11 @@ mkdir /usr/share/URLBrute/logs/dirs
 cp urlbrute.py /usr/share/URLBrute/urlbrute.py
 mv wordlists /usr/share/URLBrute
 
+echo export WORDLISTS=/usr/share/URLBrute/wordlists >> ~/.bashrc
+
 ln -s /usr/share/URLBrute/urlbrute.py /usr/bin/urlbrute
 
 python3 -m pip install -r requirements.txt
 
 echo "URLBrute installed!"
+echo "To see wordlists try: ls \$WORDLISTS"
